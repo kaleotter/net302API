32c82b78658742711ce60021e16a6e5fbdb10459
Anthony <kaleotter@gmail.com>
2018-11-27 14:15:54 +0000

    boop

--- Test1/api/models.py
+++ Test1/api/models.py
@@ -54,6 +54,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         
         full_name = '%s %s' % (self.first_name, self.last_name)
         
+        return full_name
+        
         
     def email_user(self, subject, message, from_email=None, **kwargs):
         """
